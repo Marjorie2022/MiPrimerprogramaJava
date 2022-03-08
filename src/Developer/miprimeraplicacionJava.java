9a25a2303af6ffb10c82372e5196e8b737b2e791
marjorie2022 <ramirezmaggie686@gmail.com>
2022-03-08 15:37:49 -0600

    CalculoSimple

---
+++ src/Developer/miprimeraplicacionJava.java
@@ -0,0 +1,28 @@
+package Developer;
+
+public class miprimeraplicacionJava {
+    public static void main(String [] arg) {
+        String nombreCompleto, direccion, estadoCivil;
+        int edad;
+        double gastosDiarios;
+        char genero;
+        boolean poseeVehiculo;
+        
+        nombreCompleto = "Camila Esperanza Rosales";
+        direccion = " Calle de la  armagura desvio la tristeza";
+        estadoCivil = "Soltera";
+        edad = 30;
+        gastosDiarios =34.45;  
+        genero = 'F';
+        poseeVehiculo = false;
+        
+        System.out.println(" Nombre Completo: " + nombreCompleto) ;
+        System.out.println("Direccion : " + direccion) ;
+         System.out.println("Direccion : " + direccion) ;
+          System.out.println("Edad : " + edad);
+           System.out.println("Gastos diarios : " + gastosDiarios);
+            System.out.println("Genero : " + genero);
+             System.out.println("Posee Vehiculo : " + poseeVehiculo);
+    }
+    
+}
